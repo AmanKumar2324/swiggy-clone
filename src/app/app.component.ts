9088abb718dd003b62064969ebf1513b3c91db61
Aman Kumar <aksingh230502@gmail.com>
2024-11-05 15:55:55 +0530

    Best Places section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { TopDishesComponent } from "./components/sections/top-dishes/top-dishes.component";
 import { TopRestaurantsComponent } from "./components/sections/top-restaurants/top-restaurants.component";
 import { ExploreRestaurantsComponent } from "./components/sections/explore-restaurants/explore-restaurants.component";
+import { BestPlacesComponent } from "./components/sections/best-places/best-places.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TopDishesComponent, TopRestaurantsComponent, ExploreRestaurantsComponent],
+  imports: [RouterOutlet, HeaderComponent, TopDishesComponent, TopRestaurantsComponent, ExploreRestaurantsComponent, BestPlacesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
