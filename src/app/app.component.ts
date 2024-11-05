aeb7bd23a2c31233cf81b2fcac775ca4267e557d
Aman Kumar <aksingh230502@gmail.com>
2024-11-05 15:24:30 +0530

    explore-restraunts section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TopDishesComponent } from "./components/sections/top-dishes/top-dishes.component";
 import { TopRestaurantsComponent } from "./components/sections/top-restaurants/top-restaurants.component";
+import { ExploreRestaurantsComponent } from "./components/sections/explore-restaurants/explore-restaurants.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TopDishesComponent, TopRestaurantsComponent],
+  imports: [RouterOutlet, HeaderComponent, TopDishesComponent, TopRestaurantsComponent, ExploreRestaurantsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
