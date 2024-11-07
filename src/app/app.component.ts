0721888d18bd3ef954afd7f23d678927dc0433e7
Aman Kumar <aksingh230502@gmail.com>
2024-11-07 14:34:22 +0530

    Cuisines Section Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { TopDishesComponent } from "./components/sections/top-dishes/top-dishes.
 import { TopRestaurantsComponent } from "./components/sections/top-restaurants/top-restaurants.component";
 import { ExploreRestaurantsComponent } from "./components/sections/explore-restaurants/explore-restaurants.component";
 import { BestPlacesComponent } from "./components/sections/best-places/best-places.component";
+import { CuisinesComponent } from "./components/sections/cuisines/cuisines.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TopDishesComponent, TopRestaurantsComponent, ExploreRestaurantsComponent, BestPlacesComponent],
+  imports: [RouterOutlet, HeaderComponent, TopDishesComponent, TopRestaurantsComponent, ExploreRestaurantsComponent, BestPlacesComponent, CuisinesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
